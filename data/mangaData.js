ce78c53273c18666ff909e07a5c1922e17a22806
Aya <pyon220@gmail.com>
2023-10-01 03:13:41 -0700

    added logout button

--- data/mangaData.js
+++ data/mangaData.js
@@ -6,6 +6,7 @@ const mangaData = [
       "https://m.media-amazon.com/images/I/81tadC4LSVL._AC_UF1000,1000_QL80_.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 36,
@@ -14,6 +15,7 @@ const mangaData = [
       "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1571349928l/52950513.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 37,
@@ -22,6 +24,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/it/3/37/Oshi_no_Ko_copertina.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 1,
@@ -30,6 +33,7 @@ const mangaData = [
       "https://prodimage.images-bn.com/pimages/9781421580425_p0_v1_s192x300.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 2,
@@ -37,6 +41,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/71kvo+fijnL.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 3,
@@ -45,6 +50,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg",
     price: 8.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 4,
@@ -52,6 +58,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/91M9VaZWxOL.jpg",
     price: 12.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 5,
@@ -60,6 +67,7 @@ const mangaData = [
       "https://www.rightstufanime.com/images/productImages/9781421516035_manga-Bleach-Graphic-Novel-24-primary.jpg?resizeid=3&resizeh=600&resizew=600",
     price: 10.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 6,
@@ -68,6 +76,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Death_Note_Vol_1.jpg/220px-Death_Note_Vol_1.jpg",
     price: 9.49,
     description: "",
+    category: "shonen",
   },
   {
     id: 7,
@@ -76,6 +85,7 @@ const mangaData = [
       "https://comicvine.gamespot.com/a/uploads/scale_medium/6/67663/4079417-01.jpg",
     price: 11.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 8,
@@ -84,6 +94,7 @@ const mangaData = [
       "https://comicvine.gamespot.com/a/uploads/scale_medium/6/67663/4421543-01.jpg",
     price: 13.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 9,
@@ -92,6 +103,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/en/6/69/Black_Clover%2C_volume_1.jpg",
     price: 9.79,
     description: "",
+    category: "shonen",
   },
   {
     id: 10,
@@ -99,6 +111,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/51FAgOL-1bL.jpg",
     price: 11.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 11,
@@ -106,6 +119,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/71u0UjsSrnL.jpg",
     price: 8.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 12,
@@ -114,6 +128,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/en/c/c3/OnePunchMan_manga_cover.png",
     price: 10.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 13,
@@ -121,6 +136,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/815uHbvvu1L.jpg",
     price: 11.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 14,
@@ -128,6 +144,7 @@ const mangaData = [
     image: "https://cv.bkmkn.kodansha.co.jp/9784063844160/9784063844160_w.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 15,
@@ -135,6 +152,7 @@ const mangaData = [
     image: "https://dw9to29mmj727.cloudfront.net/products/1974708101.jpg",
     price: 12.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 16,
@@ -142,6 +160,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/71vP+oDyYYL.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 17,
@@ -149,6 +168,7 @@ const mangaData = [
     image: "https://neverland-animeusa.com/assets_2m/img/comic/img_cm07.jpg",
     price: 12.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 18,
@@ -157,6 +177,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/en/c/c1/Nanatsu_no_Taizai_Volume_1.png",
     price: 11.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 19,
@@ -164,6 +185,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/81ZNkhqRvVL.jpg",
     price: 11.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 20,
@@ -171,6 +193,7 @@ const mangaData = [
     image: "http://st.cdjapan.co.jp/pictures/l/15/13/NEOBK-2262585.jpg",
     price: 19.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 21,
@@ -178,6 +201,7 @@ const mangaData = [
     image: "https://upload.wikimedia.org/wikipedia/en/4/46/Jujutsu_kaisen.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 22,
@@ -186,6 +210,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/en/b/b1/Tokyo_Revengers_volume_1_cover.jpg",
     price: 10.98,
     description: "",
+    category: "shonen",
   },
   {
     id: 23,
@@ -193,6 +218,7 @@ const mangaData = [
     image: "https://upload.wikimedia.org/wikipedia/en/1/10/Gantz_vol._1.png",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 24,
@@ -201,6 +227,7 @@ const mangaData = [
       "https://tap-multimedia-1172.nyc3.digitaloceanspaces.com/productimage/2351/9788415108443.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 25,
@@ -209,6 +236,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/en/c/cc/Rurouni_Kenshin_28.png",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 26,
@@ -217,6 +245,7 @@ const mangaData = [
       "https://m.media-amazon.com/images/I/91A2vKj1mrL._AC_UF700,800_QL80_.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 27,
@@ -225,6 +254,7 @@ const mangaData = [
       "https://upload.wikimedia.org/wikipedia/en/thumb/0/00/Monster_manga_volume_1_cover.jpg/220px-Monster_manga_volume_1_cover.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 28,
@@ -233,6 +263,7 @@ const mangaData = [
       "https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088564135/1200/9784088564135.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 29,
@@ -240,6 +271,7 @@ const mangaData = [
     image: "https://dw9to29mmj727.cloudfront.net/products/1591163250.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 30,
@@ -248,6 +280,7 @@ const mangaData = [
       "https://cdn.shopify.com/s/files/1/0282/0970/9115/products/91_XBco_adL_1200x1200.jpg?v=1619184826",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 31,
@@ -255,6 +288,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/51MQYrNqdxL.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 32,
@@ -262,6 +296,7 @@ const mangaData = [
     image: "https://m.media-amazon.com/images/I/51a4zQABJEL.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
   {
     id: 33,
@@ -270,6 +305,7 @@ const mangaData = [
       "https://dwgkfo5b3odmw.cloudfront.net/manga/thumbs/thumb-93680-OnePiece_GN100_C1_Web-3-WlH6FYTOWwrUpPtZsb4mvQ.jpg",
     price: 100.0,
     description: "",
+    category: "shonen",
   },
   {
     id: 34,
@@ -278,6 +314,7 @@ const mangaData = [
       "https://comicvine.gamespot.com/a/uploads/scale_small/11/113021/2652261-shaman_king_vol._32__shaman_king__graphic_novels____hiroyuki_takei__9781421521855__amazon.com__books___main.jpg",
     price: 9.99,
     description: "",
+    category: "shonen",
   },
 ];
 
--- pages/header.tsx
+++ pages/header.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Typography, Button, Popover } from "@mui/material";
+
 import { auth, provider } from "@/components/firebase";
 import {
   onAuthStateChanged,
@@ -13,7 +15,19 @@ import Logo from "../public/Manga.png";
 
 function Header() {
   const [user, setUser] = useState<User | null>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const imageUrl = user?.photoURL ?? "default-image-url";
+
+  const handleAvatarClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -47,16 +61,40 @@ function Header() {
   };
 
   return (
-    <nav className="w-full bg-[#fff] h-[70px] flex items-center mx-2 ">
+    <nav className="w-full bg-[#f4f4f4] h-[70px] flex items-center justify-between px-5">
+      <div className="flex flex-row items-center">
+        <Image src={Logo} height={50} alt="Manga-mart Logo" />
+      </div>
+
       {user ? (
-        <div className="flex flex-row items-center ">
-          <Image src={Logo} height={50} alt="Manga-mart Logo" />
-          <p>{user.displayName}</p>
-          <div>
+        <div className="flex flex-row items-center">
+          <p className="mr-4 text-[20px] uppercase">
+            HELLO! {user.displayName}
+          </p>
+          <div onClick={handleAvatarClick}>
             <Image src={imageUrl} alt="User Photo" width={50} height={50} />
           </div>
-
-          <button onClick={handleLogout}>Logout</button>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <Button
+              onClick={handleLogout}
+              style={{ padding: "10px", fontSize: "16px" }}
+            >
+              Logout
+            </Button>
+          </Popover>
         </div>
       ) : (
         <div>
