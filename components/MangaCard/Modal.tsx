8d5b625ce75b6946949934aa0e569232a47715a9
Aya <pyon220@gmail.com>
2023-10-06 22:33:42 -0700

    added max height to the modal window

--- components/MangaCard/Modal.tsx
+++ components/MangaCard/Modal.tsx
@@ -47,14 +47,16 @@ const Modal: React.FC<ModalProps> = ({ manga, isOpen, onRequestClose }) => {
               </div>
               {/* <!-- Modal body --> */}
               <div className="p-6 space-y-6 ">
-                <p className="text-sm sm:text-base leading-[17px] sm:leading-relaxed text-gray-500 dark:text-gray-400 overflow-y-auto h-max-[calc(80vh-10rem)]">
+                <p
+                  className="text-sm sm:text-base leading-[17px] sm:leading-relaxed text-gray-500 dark:text-gray-400 overflow-y-auto"
+                  style={{ maxHeight: "calc(80vh - 10rem)" }}
+                >
                   {manga.description}
                 </p>
               </div>
               {/* <!-- Modal footer --> */}
               <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
                 <button
-                  // data-modal-hide="defaultModal"
                   onClick={onRequestClose}
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -62,7 +64,6 @@ const Modal: React.FC<ModalProps> = ({ manga, isOpen, onRequestClose }) => {
                   I accept
                 </button>
                 <button
-                  // data-modal-hide="defaultModal"
                   onClick={onRequestClose}
                   type="button"
                   className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
