43f1b3f4238eefa60632eb42d3fa63cf9ca05cd7
Aya <pyon220@gmail.com>
2023-10-31 15:59:40 -0700

    added increase and decreas button

--- components/Cart/AddToCart.tsx
+++ components/Cart/AddToCart.tsx
@@ -15,12 +15,18 @@ interface AddToCartProps {
 }
 
 const AddToCart: React.FC<AddToCartProps> = ({ mangaData, onCartUpdated }) => {
+  const [itemCount, setItemCount] = useState<number>(0);
   const [isInCart, setIsInCart] = useState<boolean>(false);
   const { isGoogleLoggedIn, updateCart, cartItems } = useAppContext();
 
   useEffect(() => {
     setIsInCart(cartItems.some((item: MangaData) => item.id === mangaData.id));
   }, [mangaData.id, cartItems]);
+
+  useEffect(() => {
+    const count = cartItems.filter((item) => item.id === mangaData.id).length;
+    setItemCount(count);
+  }, [cartItems, mangaData.id]);
 
   console.log("Add to Cart cartItems:", cartItems);
 
@@ -32,32 +38,52 @@ const AddToCart: React.FC<AddToCartProps> = ({ mangaData, onCartUpdated }) => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
     setIsInCart(true);
 
+    setItemCount((prev) => prev + 1);
     onCartUpdated();
     updateCart();
   };
 
+  const handleIncrease = () => {
+    cartItems.push(mangaData);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    setItemCount((prev) => prev + 1);
+    onCartUpdated();
+    updateCart();
+  };
+
+  const handleDecrease = () => {
+    const index = cartItems.findIndex((item) => item.id === mangaData.id);
+    if (index !== -1) {
+      cartItems.splice(index, 1);
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+      setItemCount((prev) => prev - 1);
+      onCartUpdated();
+      updateCart();
+    }
+  };
+
   return (
     <>
-      <button
-        onClick={handleAddToCartClick}
-        className="relative inline-flex items-center justify-center p-4 px-5 py-1.5 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 "
-      >
-        <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
-        <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-        <span className="relative text-white  flex items-center">
-          {isInCart ? (
-            <>
-              <span className="text-sm sm:text-lg">Added</span>
-              {/* <FaCartArrowDown size={"20px"} className="ml-1" />{" "} */}
-            </>
-          ) : (
-            <>
-              <span className="text-sm sm:text-lg">Add to</span>
-              <FaCartArrowDown size={"20px"} className="ml-1" />{" "}
-            </>
-          )}
-        </span>
-      </button>
+      <div className="relative inline-flex items-center justify-center p-4 px-5 py-1.5 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+        {itemCount === 0 ? (
+          <button onClick={handleAddToCartClick}>
+            <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+            <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+            <span className="relative text-white  flex items-center">
+              <>
+                <span className="text-sm sm:text-lg">Add to</span>
+                <FaCartArrowDown size={"20px"} className="ml-1" />{" "}
+              </>
+            </span>
+          </button>
+        ) : (
+          <>
+            <button onClick={handleDecrease}>-</button>
+            <span>{itemCount}</span>
+            <button onClick={handleIncrease}>+</button>
+          </>
+        )}
+      </div>
     </>
   );
 };
