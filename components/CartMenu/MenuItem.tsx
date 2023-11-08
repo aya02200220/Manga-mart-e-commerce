7ed86b37b3467912fb423fbe4bce6dd03d0e2ffd
Aya <pyon220@gmail.com>
2023-11-08 05:02:13 -0800

    finished designing arangement of buttons on cartmenu

--- components/CartMenu/MenuItem.tsx
+++ components/CartMenu/MenuItem.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { motion } from "framer-motion";
 import { MangaData } from "@/types";
 import { useAppContext } from "../providers/AppContext";
+import { ImBin } from "react-icons/im";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 interface MenuItemProps {
   manga: MangaData;
@@ -28,8 +30,6 @@ const variants = {
 const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"];
 
 export const MenuItem: React.FC<MenuItemProps> = ({ manga, quantity }) => {
-  const { onCartUpdated } = useAppContext;
-
   const handleIncrement = (id) => {
     // アイテムの数量を増やすロジック
   };
@@ -44,38 +44,40 @@ export const MenuItem: React.FC<MenuItemProps> = ({ manga, quantity }) => {
 
   return (
     <motion.li
-      className="flex mb-2 pb-2 border-b border-b-1"
+      className="flex  border-b border-b-1"
       variants={variants}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.95 }}
     >
-      <div className="flex gap-3">
-        <img src={manga.image} className="w-[75px] " />
-        <div>
-          <p>{manga.title}</p>
-          <p>$ {manga.price}</p>
+      <div className="flex gap-3 w-full pt-2">
+        <img src={manga.image} className="w-[75px] h-[117px]" />
+        <div className="flex-grow flex flex-col justify-between py-3">
+          <p className="text-sm leading-[14px]">{manga.title}</p>
+          <p className="pt-2 text-lg font-semibold">
+            $ {manga.price.toFixed(2) ?? 0}
+          </p>
 
           <div className="flex items-center mt-2">
             <button
-              className="px-2 py-1 border rounded-md bg-gray-200 hover:bg-gray-300"
+              className="w-[27px] h-[27px] px-2 py-1 border rounded-md bg-gray-200 hover:bg-gray-300"
               onClick={() => handleDecrement(manga.id)}
             >
-              -
+              <AiOutlineMinus size="12px" />
             </button>
             <span className="mx-2">{quantity}</span>
             <button
-              className="px-2 py-1 border rounded-md bg-gray-200 hover:bg-gray-300"
+              className="w-[27px] h-[27px] px-2 py-1 border rounded-md bg-gray-200 hover:bg-gray-300"
               onClick={() => handleIncrement(manga.id)}
             >
-              +
+              <AiOutlinePlus size="12px" />
             </button>
           </div>
         </div>
         <button
-          className="ml-4 px-2 py-1 text-red-600 border border-red-600 rounded-md hover:bg-red-600 hover:text-white"
+          className="mt-2 w-[30px] h-[30px] flex ml-4 px-2 py-1 text-red-600 border border-red-600 rounded-md hover:bg-red-600 hover:text-white"
           onClick={() => handleRemove(manga.id)}
         >
-          Remove
+          <ImBin />
         </button>
       </div>
     </motion.li>
