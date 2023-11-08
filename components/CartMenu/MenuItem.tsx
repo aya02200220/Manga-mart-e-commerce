ca2f805d398ebcab7afa74b88a80f92e39836987
Aya <pyon220@gmail.com>
2023-11-08 04:14:42 -0800

    added qty and total on cart menu

--- components/CartMenu/MenuItem.tsx
+++ components/CartMenu/MenuItem.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { motion } from "framer-motion";
 import { MangaData } from "@/types";
+import { useAppContext } from "../providers/AppContext";
 
 interface MenuItemProps {
   manga: MangaData;
-  index: number;
+  quantity: number;
 }
 
 const variants = {
@@ -26,22 +27,57 @@ const variants = {
 
 const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"];
 
-export const MenuItem: React.FC<MenuItemProps> = ({ manga, index }) => {
-  console.log("Cart item in Menu items", manga, "index", index);
+export const MenuItem: React.FC<MenuItemProps> = ({ manga, quantity }) => {
+  const { onCartUpdated } = useAppContext;
 
-  const style = { border: `2px solid ${colors[index]}` };
+  const handleIncrement = (id) => {
+    // アイテムの数量を増やすロジック
+  };
+
+  const handleDecrement = (id) => {
+    // アイテムの数量を減らすロジック、ただし数量が1より大きい場合のみ
+  };
+
+  const handleRemove = (id) => {
+    // アイテムをリストから削除するロジック
+  };
+
   return (
     <motion.li
-      className="flex "
+      className="flex mb-2 pb-2 border-b border-b-1"
       variants={variants}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.95 }}
     >
-      <div className="">
-        <img src={manga.image} />
+      <div className="flex gap-3">
+        <img src={manga.image} className="w-[75px] " />
+        <div>
+          <p>{manga.title}</p>
+          <p>$ {manga.price}</p>
+
+          <div className="flex items-center mt-2">
+            <button
+              className="px-2 py-1 border rounded-md bg-gray-200 hover:bg-gray-300"
+              onClick={() => handleDecrement(manga.id)}
+            >
+              -
+            </button>
+            <span className="mx-2">{quantity}</span>
+            <button
+              className="px-2 py-1 border rounded-md bg-gray-200 hover:bg-gray-300"
+              onClick={() => handleIncrement(manga.id)}
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <button
+          className="ml-4 px-2 py-1 text-red-600 border border-red-600 rounded-md hover:bg-red-600 hover:text-white"
+          onClick={() => handleRemove(manga.id)}
+        >
+          Remove
+        </button>
       </div>
-      <div className="icon-placeholder" style={style} />
-      <div className="text-placeholder" style={style} />
     </motion.li>
   );
 };
--- components/CartMenu/Navigation.tsx
+++ components/CartMenu/Navigation.tsx
@@ -17,6 +17,15 @@ const variants = {
 export const Navigation = ({ isOpen }: { isOpen: boolean }) => {
   const { cartItemsCounts, updateCart, cartItems } = useAppContext();
 
+  // アイテムとその数量のマップを作成します。
+  const itemsCount = cartItems.reduce<Record<string, number>>((acc, item) => {
+    acc[item.id] = (acc[item.id] || 0) + 1;
+    return acc;
+  }, {});
+
+  // 合計金額を計算します。
+  const total = cartItems.reduce((acc, item) => acc + item.price, 0);
+
   return (
     <div className={` ${!isOpen ? "closed-menu" : ""} `}>
       <div className="absolute top-0 left-0  border border-[#333] w-full h-[50px] items-center flex">
@@ -24,28 +33,32 @@ export const Navigation = ({ isOpen }: { isOpen: boolean }) => {
         <p className="pl-[10px] font-medium">{cartItemsCounts} item</p>
       </div>
       <motion.ul
-        className={`overflow-y-auto  h-[400px]  pl-[15px] pr-[15px] absolute top-[60px] left-0 w-full  ${
+        className={`overflow-y-auto  h-[320px]  pl-[20px] pr-[20px] absolute top-[60px] left-0 w-full  ${
           !isOpen ? "closed-menu" : ""
         } `}
         variants={variants}
       >
-        {cartItems.map((manga: MangaData, index: number) => (
-          <MenuItem manga={manga} index={index} key={index} />
-        ))}
+        {Object.entries(itemsCount).map(([id, quantity]) => {
+          const item = cartItems.find((manga) => manga.id.toString() === id);
+          if (item) {
+            return <MenuItem manga={item} quantity={quantity} key={id} />;
+          }
+          return null;
+        })}
       </motion.ul>
-      <div className="absolute bottom-0 left-0  border border-[#333] w-full h-[140px] items-center justify-center flex flex-col divide-y-2">
+      <div className="absolute bottom-0 left-0  border border-[#333] w-full h-[110px] items-center justify-center flex flex-col divide-y-2">
         <div className="flex flex-1  w-full justify-between items-center">
-          <p className="pl-[10px] font-bold text-lg">Sub-total</p>
-          <p className="pr-[10px] font-medium text-lg">
-            $ {cartItemsCounts?.toFixed(2) ?? 0}
+          <p className="pl-[20px] font-bold text-md">Sub-total</p>
+          <p className="pr-[20px] font-medium text-md">
+            $ {total?.toFixed(2) ?? 0}
           </p>
         </div>
-        <div className="flex flex-1 w-full gap-8 justify-center items-center">
-          <button className="border border-black py-2 w-[120px]">
+        <div className="flex flex-1 w-full gap-4 justify-center items-center">
+          <button className="border border-black py-1 w-[120px]">
             Empty cart
           </button>
-          <button className="border border-black py-2 w-[120px]">
-            checkout
+          <button className="border border-black py-1 w-[120px]">
+            Checkout
           </button>
         </div>
       </div>
