52de4f4f93eff66561db82c9e8399bb6df9c10e0
Aya <pyon220@gmail.com>
2023-11-10 20:03:48 -0800

    adding hooks

--- components/hooks/ useCartActions.js
+++ components/hooks/ useCartActions.js
@@ -3,9 +3,7 @@ import { useAppContext } from "../providers/AppContext";
 export const useCartActions = () => {
   const { updateCart, cartItems } = useAppContext();
 
-  const incrementItem = (mangaData) => {
-    // ...handleIncrease ロジック
-  };
+  const incrementItem = (mangaData) => {};
 
   const decrementItem = (mangaData) => {
     // ...handleDecrease ロジック
