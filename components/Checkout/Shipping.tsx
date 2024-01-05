6ef31cab6cb7e0b0954ee2b9c31671502b70badd
Aya <pyon220@gmail.com>
2024-01-04 22:17:02 -0800

    added text forms for address

--- components/Checkout/Shipping.tsx
+++ components/Checkout/Shipping.tsx
@@ -52,40 +52,52 @@ export const Shipping = () => {
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
   return (
-    <div className="border border-black w-[600px] m-10 h-auto">
-      <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
-        <AccordionHeader
-          className="p-3 font-medium text-md"
-          onClick={() => handleOpen(1)}
-        >
-          01 Shipping
-        </AccordionHeader>
-        <AccordionBody>
-          <ShippingForm />
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
-        <AccordionHeader onClick={() => handleOpen(2)}>
-          How to use Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
-        <AccordionHeader onClick={() => handleOpen(3)}>
-          What can I do with Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
+    <div className="flex m-10 gap-4">
+      <div className="border border-black w-auto sm:w-2/3 h-auto">
+        <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
+          <AccordionHeader
+            className="p-3 font-medium text-md"
+            onClick={() => handleOpen(1)}
+          >
+            01 Shipping
+          </AccordionHeader>
+          <AccordionBody>
+            <ShippingForm />
+          </AccordionBody>
+        </Accordion>
+        <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
+          <AccordionHeader
+            className="p-3 font-medium text-md"
+            onClick={() => handleOpen(2)}
+          >
+            02 How to use Material Tailwind?
+          </AccordionHeader>
+          <AccordionBody>
+            We&apos;re not always in the position that we want to be at.
+            We&apos;re constantly growing. We&apos;re constantly making
+            mistakes. We&apos;re constantly trying to express ourselves and
+            actualize our dreams.
+          </AccordionBody>
+        </Accordion>
+        <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
+          <AccordionHeader
+            className="p-3 font-medium text-md"
+            onClick={() => handleOpen(3)}
+          >
+            03 What can I do with Material Tailwind?
+          </AccordionHeader>
+          <AccordionBody>
+            We&apos;re not always in the position that we want to be at.
+            We&apos;re constantly growing. We&apos;re constantly making
+            mistakes. We&apos;re constantly trying to express ourselves and
+            actualize our dreams.
+          </AccordionBody>
+        </Accordion>
+      </div>
+
+      <div className="w-auto sm:w-1/3">
+        <div className="border ">test</div>
+      </div>
     </div>
   );
 };
@@ -99,24 +111,24 @@ export const ShippingForm = () => {
     <div>
       <div className="m-5">
         <div className="border">
-          <p className="m-3">Shipping address</p>
+          <p className="m-3 text-md ">Shipping address</p>
           <p className="border border-b-4" />
 
-          <div className="flex gap-2 p-4">
+          <div className="flex flex-col sm:flex-row gap-2 pb-2 pt-4">
             <TextField
               label="First Name"
-              variant="filled"
+              // variant="filled"
               required
               size="small"
             />
             <TextField
               label="Last Name"
-              variant="filled"
+              // variant="filled"
               required
               size="small"
             />
           </div>
-          <div className=" gap-2 p-4">
+          <div className="flex flex-col gap-2 pb-2">
             <VisitorAPIComponents
               projectId={visitorApiProjectId}
               handleCountryChange={(countryObj: Country) =>
@@ -125,12 +137,50 @@ export const ShippingForm = () => {
               handleStateChange={(stateObj: State) => setState(stateObj)}
             >
               <CountryField label="Country/Territory"></CountryField>
-              <StateField label="State/Province"></StateField>
+              {/* <StateField label="State/Province"></StateField> */}
             </VisitorAPIComponents>
 
-            <hr />
-            <hr />
-            <hr />
+            <div></div>
+            <TextField
+              label="Street Address"
+              // variant="filled"
+              required
+              size="small"
+            />
+            <div className="flex gap-2">
+              <TextField
+                label="Apt/Unit"
+                // variant="filled"
+                // required
+                size="small"
+              />
+              <TextField
+                label="City"
+                // variant="filled"
+                required
+                size="small"
+              />
+            </div>
+            <div className="flex gap-2">
+              <TextField
+                label="Province"
+                // variant="filled"
+                // required
+                size="small"
+              />
+              <TextField
+                label="Postal Code"
+                // variant="filled"
+                required
+                size="small"
+              />
+            </div>
+            <TextField
+              label="Phone Number"
+              // variant="filled"
+              required
+              size="small"
+            />
           </div>
         </div>
       </div>
