e818fe59b23866cac68bd1ede765760f1c307e2a
Aya <pyon220@gmail.com>
2024-01-07 19:55:39 -0800

    adding payment form

--- components/Checkout/PaymentForm.tsx
+++ components/Checkout/PaymentForm.tsx
@@ -5,34 +5,42 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import { FaCcPaypal } from "react-icons/fa";
+import { FaRegCreditCard } from "react-icons/fa";
 
 export const PaymentForm = () => {
   return (
     <div className="m-5">
-      <div className="border bg-[#FAFAFA] ">
-        <p className="m-3 text-md font-medium">Shipping Option</p>
+      <div className="border bg-[#FAFAFA]">
+        <p className="m-3 text-md font-medium">Payment Option</p>
         <p className="border border-b-0 border-[#333]" />
 
         <div className="px-5 pt-2">
           <FormControl>
-            <RadioGroup
-              // aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="Card"
-              name="radio-buttons-group"
-            >
-              <FormControlLabel
-                value="Card"
-                control={<Radio size="small" />}
-                label="Credit Card or Debit"
-              />
-              <div className="flex">
+            <RadioGroup defaultValue="Card" name="radio-buttons-group">
+              <div className="flex items-center">
+                <FormControlLabel
+                  value="Card"
+                  control={<Radio size="small" />}
+                  label={
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <FaRegCreditCard className="mr-2" size={"20px"} />
+                      Credit Card or Debit
+                    </div>
+                  }
+                />
+              </div>
+
+              <div className="flex items-center">
                 <FormControlLabel
                   value="Paypal"
                   control={<Radio size="small" />}
-                  label="Paypal"
+                  label={
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <FaCcPaypal className="mr-2" size={"20px"} />
+                      Paypal
+                    </div>
+                  }
                 />
-
-                <FaCcPaypal />
               </div>
             </RadioGroup>
           </FormControl>
