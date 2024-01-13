f684c9dacaccaee7e0d41432efff4587124e2069
Aya <pyon220@gmail.com>
2024-01-12 21:39:31 -0800

    added back button and logo on checkout page

--- components/Checkout/Checkout.tsx
+++ components/Checkout/Checkout.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Shipping } from "./Shipping";
+import Link from "next/link";
+import Image from "next/image";
+
+import { IoIosArrowBack } from "react-icons/io";
+import Logo from "../../public/Manga.png";
 
 const Checkout = () => {
   return (
-    <div>
-      <p>checkout</p>
-      <Shipping />
-    </div>
+    <>
+      <div className="m-2 sm:m-5">
+        <Link className="fixed" href={"/"}>
+          <div className="flex items-center">
+            <IoIosArrowBack />
+            <p className="font-bold">BACK</p>
+          </div>
+        </Link>
+        <div className="flex items-center justify-center">
+          <Image src={Logo} height={50} alt="Manga-mart Logo" />
+        </div>
+        <Shipping />
+      </div>
+    </>
   );
 };
 
