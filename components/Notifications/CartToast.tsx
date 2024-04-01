6281583541e8dd76c464f9ab8500c1244f808997
Aya <pyon220@gmail.com>
2024-03-31 18:29:25 -0700

    changed resposive redigh for toast

--- components/Notifications/CartToast.tsx
+++ components/Notifications/CartToast.tsx
@@ -14,10 +14,10 @@ const CartToast: React.FC<Props> = ({ mangaData, actionType }) => {
   return (
     <div data-aos="fade-right" data-aos-duration="400">
       <div
-        className={`max-w-md w-[240px] ${bgColor} shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+        className={`max-w-md w-[100px] md:w-[240px] ${bgColor} shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5 items-center `}
       >
-        <div className="flex-1  py-6 px-2">
-          <div className="flex items-start">
+        <div className="flex-1 py-2 md:py-6 px-2 md:px-2">
+          <div className="flex flex-col md:flex-row items-start ">
             <div className="flex-shrink-0 pt-0.5 pl-3">
               <img
                 className="h-20 w-17 "
@@ -25,11 +25,13 @@ const CartToast: React.FC<Props> = ({ mangaData, actionType }) => {
                 alt={mangaData.title}
               />
             </div>
-            <div className="ml-3 flex-1 leading-4 mr-2">
+            <div className="ml-3 flex-1 leading-3 md:leading-4 mr-2 mt-2 md:mt-0 text-[12px] md:text-[16px]">
               <p className={`text-med font-bold ${textColor}`}>
                 {mangaData.title}
               </p>
-              <p className={`mt-2 text-sm leading-4 ${textColor}`}>
+              <p
+                className={`mt-1 md:mt-2 text-sm leading-[10px] md:leading-4 text-[9px] md:text-[12px] ${textColor}`}
+              >
                 {actionType === "Add"
                   ? "has been added to your cart."
                   : "has been removed from your cart."}
